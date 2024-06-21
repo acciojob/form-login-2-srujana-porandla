260caa4063d082ef32d7ad4d9f558076a4ed0cc5
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-21 15:04:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function displaInfo() {
+	var FirstName=document.getElementById('f').value;
+	var LastName=document.getElementById('l').value;
+	var PhoneNum=document.getElementById('n').value;
+	var email=document.getElementById('e').value;
+
+	var message=`First Name: ${FirstName} Last Name: ${LastName} Phone Number: ${ PhoneNum} Email ID: ${email}`;
+	alert(message);
+	
+}
